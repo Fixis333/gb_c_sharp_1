f382330842c731eb4df856644a3c20898dcc7134
Alexandr <fixis888@gmail.com>
2023-07-05 20:58:01 +0300

    HW

---
+++ Lesson_2/HW_2/1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿void NumThird (int num)
+{
+    Console.Write($"{num} -> ");
+    if (num < 100 )
+    {
+        Console.WriteLine("Третья цифра отсутствует");
+        return;
+    }
+        while (num > 999) num /= 10;
+        Console.WriteLine(num%10);
+}
+
+int num = int.Parse(Console.ReadLine()!);
+NumThird (num);
---
+++ Lesson_2/HW_2/1.3/Program.cs
@@ -0,0 +1,12 @@
+﻿void Week(int num)
+{
+    string text = "Нет такого дня недели";
+
+    if (num == 6 || num ==7) text = "Выходные";
+    else if (num < 6 && num > 0) text = "Будни";
+
+    Console.WriteLine($"{num} -> {text}");
+}
+
+int num = int.Parse(Console.ReadLine()!);
+Week(num);
