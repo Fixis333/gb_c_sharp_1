eac0a3a7f1d81b4f9a126aedaf96d068ebf792f5
Alexandr <fixis888@gmail.com>
2023-07-05 20:00:45 +0300

    Lesson_2

---
+++ Lesson_2/2.2/Program.cs
@@ -0,0 +1,15 @@
+﻿  void OneOfNum(int num_1, int num_2)
+{       
+    if (num_1 % num_2 == 0)
+    {
+        Console.WriteLine($"{num_1}, {num_2} -> multiple");
+    }
+    else
+    {
+        Console.WriteLine($"{num_1}, {num_2} -> not multiple, the remainder {num_1 % num_2}");        
+    }    
+}
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+OneOfNum(a, b);
---
+++ Lesson_2/2.3/Program.cs
@@ -0,0 +1,14 @@
+﻿void OneAndTwo(int num)
+{       
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine($"{num} -> yes");
+    }
+    else
+    {
+        Console.WriteLine($"{num} -> no");        
+    }    
+}
+
+int result = int.Parse(Console.ReadLine()!);
+OneAndTwo(result);
