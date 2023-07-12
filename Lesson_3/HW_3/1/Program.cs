f525c3753fc0524f2734e894683627eb34f7965f
Alexandr <fixis888@gmail.com>
2023-07-12 20:02:26 +0300

    HW 3

---
+++ Lesson_3/HW_3/1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите пятизначное число ");
+string N = Console.ReadLine()!;
+int Len = N.Length;
+
+if (Len == 5)
+{
+    if (N[0] == N[4] && N[1] == N[3])
+    {
+        Console.WriteLine($"{N} - Ура! Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{N} - Увы! Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {N} - не является пятизначным");
+}
