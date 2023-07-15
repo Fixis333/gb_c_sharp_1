6b6ad5fda722e22fd77dc3497c726045e5892051
Alexandr <fixis888@gmail.com>
2023-07-15 15:25:00 +0300

    HW4

---
+++ Lesson_4/HW_4/1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write( "Введите a ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write( "Введите b ");
+int b = int.Parse(Console.ReadLine()!);;
+int step = 1;
+for (int i = 1; i <= b; i++)
+    step = step * a;
+    Console.WriteLine($"Число {a} в степени {b} равно {step}");
---
+++ Lesson_4/HW_4/2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write( "Введите  число" + " ");
+int Number = int.Parse(Console.ReadLine()!);
+ 
+int Sum = 0;
+
+while (Number > 0)
+{
+    Sum = Sum + Number % 10;
+    Number /= 10;
+}
+Console.WriteLine(Sum);
