ba24a41f29e959d2ca1c6ba1309f9e1929e29edc
Alexandr <fixis888@gmail.com>
2023-07-26 20:47:25 +0300

    HW9

---
+++ Lesson_9/HW/1/Program.cs
@@ -0,0 +1,22 @@
+﻿    void EvenNumbersMtoN(int m, int n)
+    {
+    if (n < m) return;
+
+    EvenNumbersMtoN(m, n - 2);
+    Console.Write($"{n}, ");
+    }
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 2 == 0)
+{
+    EvenNumbersMtoN(M, N);
+}
+else
+{
+    EvenNumbersMtoN(M, N - 1);
+}
